c2c9eacb0ae58582aa0bdb4c2ed13a4960f0300e
Florian Zweiler <florian.zweiler@outlook.at>
2026-01-23 21:05:00 +0100

    Expose the SinglyLinkedListError

--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub const meta = @import("meta.zig");
 
 pub const SinglyLinkedList = @import("linked_list.zig").SinglyLinkedList;
+pub const SinglyLinkedListError = @import("linked_list.zig").SinglyLinkedListError;
 
 test {
     std.testing.refAllDecls(@import("tests/meta.zig"));
