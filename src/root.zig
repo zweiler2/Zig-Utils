7808235ab668e0627f72c3797376ea9a23fb0c34
Marc Zweiler <marc.zweiler@outlook.at>
2026-07-17 17:47:57 +0200

    Added `SegmentedList` type, copied from the std lib of 0.15.2

--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,7 @@ pub const meta = @import("meta.zig");
 
 pub const SinglyLinkedList = @import("linked_list.zig").SinglyLinkedList;
 pub const SinglyLinkedListError = @import("linked_list.zig").SinglyLinkedListError;
+pub const SegmentedList = @import("segmented_list.zig").SegmentedList;
 
 test {
     std.testing.refAllDecls(@import("tests/meta.zig"));
